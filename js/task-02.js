220f1b3ccd7835ff6a15c8c321d9306555cbe087
OksiWhiteleg <oksanabil99@gmail.com>
2023-07-04 20:22:44 +0200

    update-2

--- js/task-02.js
+++ js/task-02.js
@@ -7,3 +7,11 @@ const ingredients = [
   'Condiments',
 ];
 
+const listIngredients = ingredients.map(ingredient => {
+  const listIngredient = document.createElement('li');
+  listIngredient.classList.add("item");
+  listIngredient.textContent = ingredient;
+  return listIngredient;
+})
+const ulIngredients = document.querySelector('ul');
+ulIngredients.append(...listIngredients);
